a1411c88d2541d312779aaa4d51876035afbfebc
Pablo Lobeto Arenas <pablo.lobeto-arenas@capgemini.com>
2024-06-18 11:27:39 +0200

    Creado config para que inicialice el entorno

---
+++ demo/src/main/java/com/example/ioc/EntornoImpl.java
@@ -0,0 +1,26 @@
+package com.example.ioc;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EntornoImpl implements Entorno {
+	private int contador;
+	
+	public EntornoImpl(int contador) {
+		super();
+		this.contador = contador;
+	}
+
+	@Override
+	public void write(String cad) {
+		contador ++;
+		System.out.println(cad);
+	}
+
+	public int getContador() {
+		return contador;
+	}
+	
+	
+
+}
---
+++ demo/src/main/java/com/example/ioc/MyConfig.java
@@ -0,0 +1,5 @@
+package com.example.ioc;
+
+public class MyConfig {
+
+}
