6bfbb53571149020d549a6139cb1db2509edd87c
Pablo Lobeto Arenas <pablo.lobeto-arenas@capgemini.com>
2024-06-18 12:26:42 +0200

    Prueba de uso de profiles

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,14 +1,13 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.ioc.Entorno;
 import com.example.ioc.Saluda;
-import com.example.ioc.SaludaEnImpl;
+
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
@@ -18,18 +17,12 @@ public class DemoApplication implements CommandLineRunner{
 	}
 
 	@Autowired
-	@Qualifier("es")
 	Saluda saluda;
 	
 	@Autowired
-	@Qualifier("en")
-	Saluda saludaEn;
+	Saluda saluda2;
 	@Autowired
 	Entorno entorno;
-	
-	@Autowired
-	@Qualifier("en")
-	SaludaEnImpl saluda2;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -39,11 +32,11 @@ public class DemoApplication implements CommandLineRunner{
 		
 		saluda.saluda("Mundo");
 		saluda.saluda("Mundo");
-		saludaEn.saluda("World");
+		saluda2.saluda("World");
 		
 		System.out.println("saludaEs: " +saluda.getContador());
 		System.out.println("entorno: " +entorno.getContador());
-		System.out.println("saludaEn: " + saludaEn.getContador());
+		
 		System.out.println("saluda2: " + saluda2.getContador());
 	}
 
--- demo/src/main/java/com/example/ioc/SaludaImpl.java
+++ demo/src/main/java/com/example/ioc/SaludaImpl.java
@@ -2,12 +2,14 @@ package com.example.ioc;
 
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("saludaEs")
 @Qualifier("es")
 @Scope("prototype")
+@Profile("es")
 public class SaludaImpl implements Saluda {
 	Entorno entorno;
 	
