f38eebbc45131a48764039fc2c72a1b6ffcf2631
Pablo Lobeto Arenas <pablo.lobeto-arenas@capgemini.com>
2024-06-19 13:16:54 +0200

    Utilizar clases nested y display name

--- demo-maven/src/test/java/com/example/CalculadoraTest.java
+++ demo-maven/src/test/java/com/example/CalculadoraTest.java
@@ -6,9 +6,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Pruebas de la clase calculadora")
 class CalculadoraTest {
+
+	Calculadora calculadora;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -20,53 +25,74 @@ class CalculadoraTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		calculadora = new Calculadora();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
-	@Test
-	void testAdd() {
-		var calculadora = new Calculadora();
-		
-		var result = calculadora.add(1, 2);
-		
-		assertEquals(3, result);
+	@Nested
+	@DisplayName("Metodo add")
+	class Add {
+		@Nested
+		class OK {
+			@Test
+			@DisplayName("Suma dos enteros")
+			void testAdd() {
+				assertEquals(3, calculadora.add(1, 2));
+			}
+
+			@Test
+			@DisplayName("Suma dos reales")
+			void testAdd2() {
+
+				var result = calculadora.add(1.24, 2.24);
+
+				assertEquals(3.48, result);
+			}
+		}
+
+		@Nested
+		class KO {
+
+		}
+
 	}
-	
-	@Test
-	void testAddDouble() {
-		var calculadora = new Calculadora();
-		
-		var result = calculadora.add(1.24, 2.24);
-		
-		assertEquals(3.48, result);
-	}
-	
-	@Test
-	void testDiv() {
-		var calculadora = new Calculadora();
-		
-		var result = calculadora.div(3, 2);
-		
-		assertEquals(1, result);
-	}
-	
-	@Test
-	void testDivReal() {
-		var calculadora = new Calculadora();
-		
-		var result = calculadora.div(3.0, 2.0);
-		
-		assertEquals(1.5, result);
-	}
-	
-	@Test
-	void testDivPorCeroReal() {
-		var calculadora = new Calculadora();
-		
-		assertThrows(ArithmeticException.class, () -> calculadora.div(3.0, 0.0));
+
+	@Nested
+	@DisplayName("Metodo div")
+	class Div {
+		@Nested
+		class OK {
+			@Test
+			@DisplayName("Division de enteros")
+			void testDiv() {
+
+				var result = calculadora.div(3, 2);
+
+				assertEquals(1, result);
+			}
+
+			@Test
+			@DisplayName("Division de reales")
+			void testDiv2() {
+
+				var result = calculadora.div(3.0, 2.0);
+
+				assertEquals(1.5, result);
+			}
+		}
+
+		@Nested
+		class KO {
+			@Test
+			@DisplayName("Division reales por 0")
+			void testDiv3() {
+
+				assertThrows(ArithmeticException.class, () -> calculadora.div(3.0, 0.0));
+			}
+		}
 	}
 
 }
