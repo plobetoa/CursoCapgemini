cf12b5c55a97be13a1d21fdf5e6072d64f9f24e0
Pablo Lobeto Arenas <pablo.lobeto-arenas@capgemini.com>
2024-06-26 11:13:19 +0200

    Agregados test de language service y category service

--- catalogo/src/test/java/com/example/domains/services/CategoryServiceImplTest.java
+++ catalogo/src/test/java/com/example/domains/services/CategoryServiceImplTest.java
@@ -10,20 +10,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import com.example.domains.contracts.repositories.FilmRepository;
-import com.example.domains.entities.Film;
+import com.example.domains.contracts.repositories.CategoryRepository;
+import com.example.domains.entities.Category;
 import com.example.exceptions.DuplicateKeyException;
 import com.example.exceptions.InvalidDataException;
 import com.example.exceptions.NotFoundException;
 
-class FilmServiceImplTest {
+class CategoryServiceImplTest {
 	
-	FilmServiceImpl srv;
-	FilmRepository dao;
+	CategoryServiceImpl srv;
+	CategoryRepository dao;
 	@BeforeEach
 	void preparation() {
-		dao = mock(FilmRepository.class);
-		srv = new FilmServiceImpl(dao);
+		dao = mock(CategoryRepository.class);
+		srv = new CategoryServiceImpl(dao);
 	}
 
 	@Nested
@@ -34,7 +34,7 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Agregar correcta")
 			void test1(){
-				var test = mock(Film.class);
+				var test = mock(Category.class);
 				when(test.isValid()).thenReturn(true);
 				try {
 					srv.add(test);
@@ -52,7 +52,7 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Datos incorrectos, no es valido")
 			void test1(){
-				var test = mock(Film.class);
+				var test = mock(Category.class);
 				when(test.isInvalid()).thenReturn(true);
 				assertThrows(InvalidDataException.class, () -> srv.add(test));
 			}
@@ -60,14 +60,14 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Datos incorrectos, es null")
 			void test2() {
-				Film test = null;
+				Category test = null;
 				assertThrows(InvalidDataException.class, () -> srv.add(test));
 			}
 			
 			@Test
 			@DisplayName("Datos incorrectos, ya esta añadido")
 			void test3() {
-				var test = mock(Film.class);
+				var test = mock(Category.class);
 				when(test.isInvalid()).thenReturn(false);
 				when(dao.existsById(anyInt())).thenReturn(true);
 				assertThrows(DuplicateKeyException.class, () -> srv.add(test));
@@ -85,9 +85,9 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Datos correcta")
 			void test1(){
-				var test = mock(Film.class);
+				var test = mock(Category.class);
 				when(test.isValid()).thenReturn(true);
-				when(test.getFilmId()).thenReturn(1);
+				when(test.getCategoryId()).thenReturn(1);
 				when(dao.existsById(anyInt())).thenReturn(true);
 					try {
 						srv.modify(test);
@@ -105,7 +105,7 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Datos incorrectos,no es valido")
 			void test1(){
-				var test = mock(Film.class);
+				var test = mock(Category.class);
 				when(test.isInvalid()).thenReturn(true);
 				assertThrows(InvalidDataException.class, () -> srv.modify(test));
 			}
@@ -113,16 +113,16 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Datos incorrectos, es null")
 			void test2() {
-				Film test = null;
+				Category test = null;
 				assertThrows(InvalidDataException.class, () -> srv.modify(test));
 			}
 			
 			@Test
 			@DisplayName("Datos incorrectos,no esta añadido id distito de 0")
 			void test3() {
-				var test = mock(Film.class);
+				var test = mock(Category.class);
 				when(test.isInvalid()).thenReturn(false);
-				when(test.getFilmId()).thenReturn(1);
+				when(test.getCategoryId()).thenReturn(1);
 				when(dao.existsById(anyInt())).thenReturn(false);
 				assertThrows(NotFoundException.class, () -> srv.modify(test));
 			}
@@ -130,9 +130,9 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Datos incorrectos, no esta añadido id 0")
 			void test4() {
-				var test = mock(Film.class);
+				var test = mock(Category.class);
 				when(test.isInvalid()).thenReturn(false);
-				when(test.getFilmId()).thenReturn(0);
+				when(test.getCategoryId()).thenReturn(0);
 				assertThrows(NotFoundException.class, () -> srv.modify(test));
 			}
 			
@@ -148,7 +148,7 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Eliminar correcto")
 			void test1(){
-				var test = new Film(1);
+				var test = new Category(1);
 					try {
 						srv.delete(test);
 					} catch (InvalidDataException e) {
@@ -163,7 +163,7 @@ class FilmServiceImplTest {
 			@Test
 			@DisplayName("Datos incorrectos, es null")
 			void test2() {
-				Film test = null;
+				Category test = null;
 				assertThrows(InvalidDataException.class, () -> srv.delete(test));
 			}
 			
