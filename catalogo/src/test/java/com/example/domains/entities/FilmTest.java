cd516a27b53543376a39493481c7da2c335d55ce
Pablo Lobeto Arenas <pablo.lobeto-arenas@capgemini.com>
2024-06-26 11:23:24 +0200

    Casos de prueba para film, language y category

---
+++ catalogo/src/test/java/com/example/domains/entities/FilmTest.java
@@ -0,0 +1,54 @@
+package com.example.domains.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class FilmTest {
+
+    @Nested
+    @DisplayName("Validaciones de atributos")
+    class Validations {
+        @Nested
+        class OK {
+            @Test
+            @DisplayName("Datos correctos")
+            void test1() {
+                var test = new Film(1, 120, "R", (byte) 3, new BigDecimal("4.99"), new BigDecimal("19.99"));
+                test.setTitle("Inception");
+                test.setLanguage(new Language(1));
+                assertTrue(test.isValid());
+            }
+        }
+
+        @Nested
+        class KO {
+            @ParameterizedTest(name = "Caso {index}: {0} - {1} - {2} - {3} - {4} - {5} - {6}")
+            @DisplayName("Datos incorrectos")
+            @CsvSource({
+                "1, 120, R, 3, 4.99, 19.99, ''", // Title vacío
+                "1, 120, R, 3, 4.99, 19.99, 'A'", // Title muy corto
+                "1, 120, R, 3, 4.99, 19.99, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'", // Title muy largo
+                "1, -1, R, 3, 4.99, 19.99, 'Inception'", // Length negativo
+                "1, 120, R, -1, 4.99, 19.99, 'Inception'", // RentalDuration negativo
+                "1, 120, R, 3, -4.99, 19.99, 'Inception'", // RentalRate negativo
+                "1, 120, R, 3, 4.99, -19.99, 'Inception'", // ReplacementCost negativo
+                "1, 120, , 3, 4.99, 19.99, 'Inception'", // Rating vacío
+                "1, 120, RR, 3, 4.99, 19.99, 'Inception'", // Rating con longitud inválida
+                "1, 120, R, 3, 4.99, 19.99, 'Inception', null" // Language nulo
+            })
+            void test1(int filmId, int length, String rating, byte rentalDuration, BigDecimal rentalRate, BigDecimal replacementCost, String title) {
+                var test = new Film(filmId, length, rating, rentalDuration, rentalRate, replacementCost);
+                test.setTitle(title);
+                test.setLanguage(null);
+                assertTrue(test.isInvalid());
+            }
+        }
+    }
+
+}
