591b3d26b00a2a070eb363c083174193477aa2df
Pablo Lobeto Arenas <pablo.lobeto-arenas@capgemini.com>
2024-07-10 14:07:25 +0200

    Log a consola y notificación a usuario

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
 import { MyCoreModule } from '@my/core';
+import { NotificationComponent } from './main/notification/notification.component';
+import { DemosComponent } from './demos/demos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, MyCoreModule],
+  imports: [RouterOutlet, SecurityModule, MyCoreModule,  NotificationComponent, DemosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ curso/src/lib/my-core/services/logger.service.ts
@@ -0,0 +1,38 @@
+import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
+
+export const ERROR_LEVEL = new InjectionToken<string>('ERROR_LEVEL')
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggerService {
+
+  private readonly nivel: number
+
+  constructor(@Optional() @Inject(ERROR_LEVEL) nivel?: number){
+    //if(nivel){
+    //  this.nivel = nivel
+    //}
+    this.nivel = nivel ?? 99
+  }
+  public error(message: string): void{
+    if(this.nivel >0)
+      console.error(message)
+  }
+  public warn(message: string): void{
+    if(this.nivel >3)
+      console.warn(message)
+  }
+  public info(message: string): void{
+    if(this.nivel >2)
+      if(console.info){
+        console.info(message)
+      }   else {
+        console.log(message)
+      }
+  }
+  public log(message: string): void{
+    if(this.nivel >3)
+      console.log(message)
+  }
+}
