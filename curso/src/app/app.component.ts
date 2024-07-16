c281fbbea2709c91eb49bb3e6f1620b3281eb065
Pablo Lobeto Arenas <pablo.lobeto-arenas@capgemini.com>
2024-07-16 08:34:45 +0200

    Creadi header and footer

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -5,11 +5,15 @@ import { NotificationComponent } from './main/notification/notification.componen
 import { DemosComponent } from './demos/demos.component';
 import { HomeComponent, NotificationModalComponent } from './main';
 import { DashboardComponent } from "./ejemplos/dashboard/dashboard.component";
+import { HeaderComponent } from './main/header/header.component';
+import { FooterComponent } from './main/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, NotificationComponent, NotificationModalComponent, DemosComponent, HomeComponent, DashboardComponent],
+  imports: [RouterOutlet, SecurityModule, NotificationComponent,
+     NotificationModalComponent, DemosComponent, HomeComponent, DashboardComponent,
+    HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
